ee51500e629aed4eb04755249c5465712c17a91d
YounglanHong <yong2401@gmail.com>
2021-04-28 16:48:30 +0900

    🔨 Add react, ts, sass and create template

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">react-typescript-sass-boilerplate</header>
+    </div>
+  );
+}
+
+export default App;
